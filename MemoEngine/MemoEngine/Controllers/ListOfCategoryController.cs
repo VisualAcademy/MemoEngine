5828d95d7534c3804c759fd21ddead422eefd51e
WebCampKR <webcamp@outlook.kr>
2018-12-18 00:45:46 +0900

    강의 소스 ListOfCategoryController 추가

---
+++ MemoEngine/MemoEngine/Controllers/ListOfCategoryController.cs
@@ -0,0 +1,18 @@
+﻿using MemoEngine.Models;
+using System.Web.Mvc;
+
+namespace MemoEngine.Controllers
+{
+    public class ListOfCategoryController : Controller
+    {
+        // GET: ListOfCategory
+        public ActionResult Index()
+        {
+            var categoryRepository = new CategoryRepositoryInMemory();
+
+            var categories = categoryRepository.GetCategories();
+
+            return View(categories);
+        }
+    }
+}
