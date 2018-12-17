14269d6bab27b5f1bcbeafb45ef592255a42cadc
WebCampKR <webcamp@outlook.kr>
2018-12-18 00:40:35 +0900

    닷넷스탠다드 프로젝트 생성 및 참조 후 테스트

---
+++ MemoEngine/MemoEngine/Samples/FrmListOfCategory.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmListOfCategory.aspx.cs" Inherits="MemoEngine.Samples.FrmListOfCategory" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>카테고리 리스트</h1>
+            <asp:GridView ID="ctlCategoryList" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MemoEngine/MemoEngine/Samples/FrmListOfCategory.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.UI;
+using MemoEngine.Models; 
+
+namespace MemoEngine.Samples
+{
+    public partial class FrmListOfCategory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                DisplayData();
+            }
+        }
+
+        private void DisplayData()
+        {
+            var categoryRepository = new CategoryRepositoryInMemory();
+
+            this.ctlCategoryList.DataSource = categoryRepository.GetCategories();
+            this.ctlCategoryList.DataBind();
+        }
+    }
+}
