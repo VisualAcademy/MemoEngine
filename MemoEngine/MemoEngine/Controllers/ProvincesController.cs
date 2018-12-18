aaf263f5cde737da2cc2e69ae9d4e9ed13d6244e
WebCampKR <webcamp@outlook.kr>
2018-12-19 04:10:26 +0900

    데모_Entity Framework_MVC 5_스캐폴딩_CRUD 페이지를 자동 생성

---
+++ MemoEngine/MemoEngine/Controllers/ProvincesController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using MemoEngine.Models;
+
+namespace MemoEngine.Controllers
+{
+    public class ProvincesController : Controller
+    {
+        private MemoEngineContext db = new MemoEngineContext();
+
+        // GET: Provinces
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Provinces.ToListAsync());
+        }
+
+        // GET: Provinces/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Province province = await db.Provinces.FindAsync(id);
+            if (province == null)
+            {
+                return HttpNotFound();
+            }
+            return View(province);
+        }
+
+        // GET: Provinces/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Provinces/Create
+        // 초과 게시 공격으로부터 보호하려면 바인딩하려는 특정 속성을 사용하도록 설정하십시오. 
+        // 자세한 내용은 https://go.microsoft.com/fwlink/?LinkId=317598을(를) 참조하십시오.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,ProvinceName,LocationName,LocationCode,Latitude,Longitude,Elevation")] Province province)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Provinces.Add(province);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(province);
+        }
+
+        // GET: Provinces/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Province province = await db.Provinces.FindAsync(id);
+            if (province == null)
+            {
+                return HttpNotFound();
+            }
+            return View(province);
+        }
+
+        // POST: Provinces/Edit/5
+        // 초과 게시 공격으로부터 보호하려면 바인딩하려는 특정 속성을 사용하도록 설정하십시오. 
+        // 자세한 내용은 https://go.microsoft.com/fwlink/?LinkId=317598을(를) 참조하십시오.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,ProvinceName,LocationName,LocationCode,Latitude,Longitude,Elevation")] Province province)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(province).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(province);
+        }
+
+        // GET: Provinces/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Province province = await db.Provinces.FindAsync(id);
+            if (province == null)
+            {
+                return HttpNotFound();
+            }
+            return View(province);
+        }
+
+        // POST: Provinces/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Province province = await db.Provinces.FindAsync(id);
+            db.Provinces.Remove(province);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MemoEngine/MemoEngine/Models/Province.cs
@@ -0,0 +1,43 @@
+﻿namespace MemoEngine.Models
+{
+    /// <summary>
+    /// 지방 모델
+    /// </summary>
+    public class Province
+    {
+        /// <summary>
+        /// 번호
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 지자체명
+        /// </summary>
+        public string ProvinceName { get; set; }
+
+        /// <summary>
+        /// 지역명
+        /// </summary>
+        public string LocationName { get; set; }
+
+        /// <summary>
+        /// 지역코드
+        /// </summary>
+        public string LocationCode { get; set; }
+
+        /// <summary>
+        /// 위도
+        /// </summary>
+        public float Latitude { get; set; }
+
+        /// <summary>
+        /// 경도
+        /// </summary>
+        public float Longitude { get; set; }
+
+        /// <summary>
+        /// 해발 높이, 고도
+        /// </summary>
+        public string Elevation { get; set; }
+    }
+}
