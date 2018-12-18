6b283840f52397aaddcf52e0b084e021aaccb3f7
WebCampKR <webcamp@outlook.kr>
2018-12-19 05:49:45 +0900

    SQL Server Database Project_Provinces

---
+++ MemoEngine/MemoEngine.Database/dbo/Tables/Provinces.sql
@@ -0,0 +1,11 @@
+﻿-- 지방 테이블: 지역별 위도경도
+CREATE TABLE [dbo].[Provinces] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,				-- 번호
+    [ProvinceName] NVARCHAR (MAX) NULL,									-- 지자체명
+    [LocationName] NVARCHAR (MAX) NULL,
+    [LocationCode] NVARCHAR (MAX) NULL,
+    [Latitude]     REAL           NOT NULL,
+    [Longitude]    REAL           NOT NULL,
+    [Elevation]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Provinces] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
