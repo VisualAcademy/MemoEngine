dc9705dea0556ac940fee7c9610bc4ee7228f544
WebCampKR <webcamp@outlook.kr>
2018-12-22 07:54:26 +0900

    강의_매개변수를 갖는 Web API를 jQuery Ajax로 호출하기 데모

---
+++ MemoEngine/MemoEngine/Controllers/WeatherByProvinceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MemoEngine.Controllers
+{
+    public class WeatherByProvinceController : Controller
+    {
+        // GET: WeatherByProvince
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MemoEngine/MemoEngine/Controllers/WeatherByProvincesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace MemoEngine.Controllers
+{
+    public class WeatherByProvincesController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Weather> Get(string locationName)
+        {
+            List<Weather> weathers = new List<Weather>();
+
+            weathers.Add(new Weather { Num = 1, Location = "서울", Cloud = 1234 });
+            weathers.Add(new Weather { Num = 2, Location = "서울", Cloud = 2345 });
+            weathers.Add(new Weather { Num = 3, Location = "서울", Cloud = 3456 });
+            weathers.Add(new Weather { Num = 1, Location = "부산", Cloud = 3456 });
+            weathers.Add(new Weather { Num = 2, Location = "부산", Cloud = 4567 });
+
+            return weathers.Where(w => w.Location == locationName).ToList(); 
+        }
+    }
+
+    public class Weather
+    {
+        public int Num { get; set; }
+        public string Location { get; set; }
+        public float Cloud { get; set; }
+    }
+}
