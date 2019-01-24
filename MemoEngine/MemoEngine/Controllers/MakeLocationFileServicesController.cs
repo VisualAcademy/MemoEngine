c7a6012f5aaa0c5d7d0689722cfe111236878999
RedPlus <redplus@redplus.net>
2019-01-24 11:52:12 +0900

    jQuery Ajax를 사용하여 서버 쪽에 텍스트파일 생성하기

---
+++ MemoEngine/MemoEngine/Controllers/MakeLocationFileServicesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Http;
+
+namespace MemoEngine.Controllers
+{
+    /// <summary>
+    /// jQuery Ajax를 사용하여 서버 쪽에 텍스트파일 생성하기
+    /// </summary>
+    public class MakeLocationFileServicesController : ApiController
+    {
+        [HttpPost]
+        [Route("api/MakeLocationFileServices/PostMakeFile")]
+        public void PostMakeFile(int id)
+        {
+            var folder = HttpContext.Current.Server.MapPath("/BoardFiles/Projects");
+            var file = Path.Combine(folder, Guid.NewGuid().ToString() + ".dat");
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                writer.WriteLine(id.ToString());
+            }
+        }
+    }
+}
